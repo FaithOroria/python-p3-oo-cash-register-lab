f5eb8f2119ff50dbb17cc47839f09f71887a7967
Faith Achieng <faith.achieng@student.moringaschool.com>
2023-10-26 14:53:41 +0300

    Assignment

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,31 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+
+    def add_item(self, title, price, quantity=1):
+        for _ in range(quantity):
+            self.items.append(title)
+            self.total += price
+
+    def apply_discount(self):
+        if self.discount > 0:
+            self.total -= (self.total * self.discount / 100)
+            print(f"After the discount, the total comes to ${self.total:.2f}.")
+        else:
+            print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+        if self.items:
+            last_item_price = self.total
+            self.total -= last_item_price
+            self.items.pop()
+        if not self.items:
+            self.total = 0.0  # Set total to 0.0 if there are no items
+
+
+
+  
+
+
